c668a7ac249257bbd3a25be498e4feeb868c0536
Quentin Casasnovas <quentin.casasnovas@vates.tech>
2026-02-23 07:35:10 +0100

    kabi: properly pretty-print enums.
    Signed-off-by: Quentin Casasnovas <quentin.casasnovas@vates.tech>

--- scripts/kabi/src/kabi/symtypes.py
+++ scripts/kabi/src/kabi/symtypes.py
@@ -65,7 +65,7 @@ class SymTypes:
         result: list[str] = []
         for tok in self.symtok[token]:
             result.append(self.name(tok))
-        return pretty(" ".join(result))
+        return pretty(" ".join(result), enum_mode=token.startswith("e#"))
 
     def crc(self, token: str, fn: str | None = None) -> int:
         if not fn:
