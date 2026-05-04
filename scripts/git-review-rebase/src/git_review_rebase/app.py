2024ff58d627db62cc75b0e7c38ddf5181212250
Quentin Casasnovas <quentin.casasnovas@vates.tech>
2026-05-04 15:38:17 +0200

    git-review-rebase: factor out _open_diff.
    This is a pre-requisite change to allowing opening a side-by-side diff with
    a different left commit, useful for newly added commits when we want to
    compare them with the upstream commit they were cherry-pick'ed from,
    instead of from nothing on the original range that was rebased.
    
    Signed-off-by: Quentin Casasnovas <quentin.casasnovas@vates.tech>

--- scripts/git-review-rebase/src/git_review_rebase/app.py
+++ scripts/git-review-rebase/src/git_review_rebase/app.py
@@ -154,25 +154,30 @@ class GitReviewRebase(App):
         self.rebase_table.focus()
         self.refresh_bindings()
 
-    async def on_data_table_row_selected(self, event: DataTable.RowSelected) -> None:
+    async def _open_diff(
+        self,
+        left_commit: pygit2.Commit | None,
+        right_commit: pygit2.Commit | None,
+    ) -> None:
         assert self.diff_table is not None
+        assert self.rebase_table is not None
+        self.rebase_table.styles.height = "20%"
+        self.diff_table.styles.height = "80%"
+        async with self.diff_table.reload_lock:
+            self.diff_table.load_commit_diff(left_commit, right_commit)
+            await self.diff_table.show_diff()
+        self.diff_table.focus()
+        self.active_table = self.diff_table
+        self.refresh()
+
+    async def on_data_table_row_selected(self, event: DataTable.RowSelected) -> None:
         assert self.rebase_table is not None
 
         if event.data_table == self.rebase_table:
-            assert event.row_key.value is not None
-            self.rebase_table.styles.height = "20%"
-            self.diff_table.styles.height = "80%"
-            async with self.diff_table.reload_lock:
-                assert self.rebased_commits_matches is not None
-                assert isinstance(event.row_key.value, pygit2.Oid)
-                self.diff_table.load_commit_diff(
-                    self.rebased_commits_matches.commit_matches[event.row_key.value].left_commit,
-                    self.rebased_commits_matches.commit_matches[event.row_key.value].right_commit,
-                )
-                await self.diff_table.show_diff()
-            self.diff_table.focus()
-            self.active_table = self.diff_table
-            self.refresh()
+            assert self.rebased_commits_matches is not None
+            assert isinstance(event.row_key.value, pygit2.Oid)
+            match = self.rebased_commits_matches.commit_matches[event.row_key.value]
+            await self._open_diff(match.left_commit, match.right_commit)
 
     def on_close_diff_table(self, message: CloseDiffTable):
         assert self.diff_table is not None
